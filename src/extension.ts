8aedae3f52391d4bfdb9be33044088b709369603
bodoganabel <bodoganabel@gmail.com>
2023-08-31 18:10:34 +0200

    Copy file to chatgpt command works

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "mvp-productivity-tools" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mvp-productivity-tools" is now active!');
+  let helloDisposable = vscode.commands.registerCommand('mvp-productivity-tools.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from MVP Productivity Tools!');
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mvp-productivity-tools.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MVP Productivity Tools!');
-	});
+  let copyDisposable = vscode.commands.registerCommand('mvp-productivity-tools.copyFileForChatGPT', async (uri?: vscode.Uri, editor?: vscode.TextEditor) => {
+    let filePath = '';
 
-	context.subscriptions.push(disposable);
+    if (uri) {
+      filePath = uri.fsPath;
+    } else if (editor) {
+      filePath = editor.document.uri.fsPath;
+    } else if (vscode.window.activeTextEditor) {
+      filePath = vscode.window.activeTextEditor.document.uri.fsPath;
+    } else {
+      vscode.window.showErrorMessage('No file selected.');
+      return;
+    }
+
+    const fileName = path.basename(filePath);
+
+    try {
+      const fileContent = await fs.promises.readFile(filePath, 'utf8');
+      const clipboardContent = `Here is [${fileName}]\n\`${fileContent}\``;
+      
+      await vscode.env.clipboard.writeText(clipboardContent);
+      vscode.window.showInformationMessage('File name and content copied to clipboard.');
+    } catch (err) {
+      vscode.window.showErrorMessage('Failed to copy file name and content to clipboard.');
+      console.error(err);
+    }
+  });
+
+  context.subscriptions.push(helloDisposable, copyDisposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
